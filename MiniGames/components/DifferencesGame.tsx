ed7d6782da8862605bd2d64aa700c9548cbe9c65
Immateriall <flavien.foussat@gmail.com>
2019-02-03 12:58:55 +0100

    Jeux des différences

--- MiniGames/components/DifferencesGame.tsx
+++ MiniGames/components/DifferencesGame.tsx
@@ -1,170 +1,144 @@
-import {
-  Button,
-  TextInput,
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity
-} from "react-native";
+import { difference } from "lodash";
 import React, { Component, ReactElement } from "react";
-import { SquareShape } from "../components/SquareShape";
-import { RectangleShape } from "../components/RectangleShape";
-import { TriangleShape } from "../components/TriangleShape";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
 import { CircleShape } from "../components/CircleShape";
 import { DiamondShape } from "../components/DiamondShape";
-import { Spacer } from "../components/Spacer";
+import { RectangleShape } from "../components/RectangleShape";
+import { SquareShape } from "../components/SquareShape";
+import { TriangleShape } from "../components/TriangleShape";
 
 type DifferencesGameState = {
-  //Nombre de figures à afficher
-  numberShapes: number;
   score: number;
   //Tableau pour connaître les paires sélectionnées
-  isSelected: any;
-  //Tableau contenant les paires de figures
-  scene: any;
-  isCreated: boolean;
+  isLineSelected: boolean[];
 };
+
+//Nombre de figures à afficher
+const numberShapes = 7;
+
+type Pair = {
+  isCorrect: boolean;
+  shape1: JSX.Element;
+  shape2: JSX.Element;
+  id: number;
+};
+
+const shapes = [
+  <SquareShape />,
+  <RectangleShape />,
+  <TriangleShape />,
+  <CircleShape />,
+  <DiamondShape />
+];
+
 export class DifferencesGame extends React.Component<{}, DifferencesGameState> {
+  private scene: Pair[] = [];
+
   constructor(props: any) {
     super(props);
     this.state = {
-      numberShapes: 7,
       score: 0,
-      isSelected: this.initArray(),
-      scene: [],
-      isCreated: false
+      isLineSelected: new Array(numberShapes).fill(false)
     };
   }
 
   render() {
-    if (!this.state.isCreated) {
-      this.setState({
-        scene: this.createScene()
-      });
+    if (this.scene.length == 0) {
+      this.scene = this.createScene();
     }
+
     return (
       <View style={styles.container}>
         <Text style={{ fontWeight: "bold" }}> Score: {this.state.score}</Text>
-        {this.state.scene}
+        {this.renderScene()}
       </View>
     );
   }
 
-  //On initialise toutes les lignes à false
-  private initArray() {
-    var row = 0;
-    var numberShapes = 7;
-    var array = [];
-
-    while (row != numberShapes) {
-      array.push(false);
-      row += 1;
-    }
-    window.console.log("test = " + array[3]);
-    return (array);
+  private renderScene() {
+    return this.scene.map(pair => {
+      if (pair.isCorrect) {
+        return (
+          <TouchableOpacity
+            onPress={() => this.setScore(1, pair.id)}
+            style={
+              this.state.isLineSelected[pair.id]
+                ? styles.correct
+                : styles.default
+            }
+            disabled={this.state.isLineSelected[pair.id]}
+            key={"Space" + pair.id}
+          >
+            {pair.shape1}
+            <View style={{ width: 150, height: 50 }} />
+            {pair.shape2}
+          </TouchableOpacity>
+        );
+      } else {
+        return (
+          <TouchableOpacity
+            onPress={() => this.setScore(0, pair.id)}
+            style={
+              this.state.isLineSelected[pair.id] ? styles.wrong : styles.default
+            }
+            disabled={this.state.isLineSelected[pair.id]}
+            key={"Space" + pair.id}
+          >
+            {pair.shape1}
+            <View style={{ width: 150, height: 50 }} />
+            {pair.shape1}
+          </TouchableOpacity>
+        );
+      }
+    });
   }
 
   //Créer une scène avec des figures
   private createScene() {
-    var row = 0;
-    var scene = [];
-
-    //Tant que la scène n'est pas entièrement créée
-    while (row != this.state.numberShapes) {
+    let scene: Pair[] = [];
+    for (let index = 0; index < numberShapes; index++) {
       //On choisit aléatoirement 1 figure parmis 5
-      var rand = 1 + Math.floor(Math.random() * 5);
+      const randomShape = this.getRandomShape(null);
       //Une chance sur deux qu'il y est une différence sur la paire
-      var randDiff = Math.floor(Math.random() * 2);
+      const isCorrect = Math.random() >= 0.5;
 
-      switch (rand) {
-        case 1:
-          scene.push(this.makePair(row, <SquareShape />, randDiff));
-          break;
-        case 2:
-          scene.push(this.makePair(row, <RectangleShape />, randDiff));
-          break;
-        case 3:
-          scene.push(this.makePair(row, <TriangleShape />, randDiff));
-          break;
-        case 4:
-          scene.push(this.makePair(row, <CircleShape />, randDiff));
-          break;
-        case 5:
-          scene.push(this.makePair(row, <DiamondShape />, randDiff));
-          break;
-      }
-      row += 1;
+      const pair: Pair = {
+        id: index,
+        isCorrect,
+        shape1: randomShape,
+        shape2: this.getRandomShape(randomShape)
+      };
+
+      scene.push(pair);
     }
-    this.setState({
-        isCreated: true
-      });
     return scene;
-  }
-
-  //Créer une paire de figure
-  private makePair(row: number, shape: any, randDiff: number) {
-    var pair;
-    window.console.log("row = " + row + "tab[row] = " + this.state.isSelected[row]);
-    //Si il doit y avoir une différence dans la paire
-    if (randDiff) {
-      pair = (
-        <TouchableOpacity
-          onPress={() => this.setScore(1, row)}
-          style={this.state.isSelected[row] ? styles.correct : styles.correct}
-          disabled={this.state.isSelected[row]}
-        >
-          {shape}
-          <View key={"Space" + row} style={{ width: 150, height: 50 }} />
-          {this.getRandomShape(shape)}
-        </TouchableOpacity>
-      );
-    } else {
-      pair = (
-        <TouchableOpacity
-          onPress={() => this.setScore(0, row)}
-          style={this.state.isSelected[row] ? styles.wrong : styles.correct}
-          disabled={this.state.isSelected[row]}
-        >
-          {shape}
-          <View key={"Space" + row} style={{ width: 150, height: 50 }} />
-          {shape}
-        </TouchableOpacity>
-      );
-    }
-    return pair;
   }
 
   //Retourne une figure aléatoire différente de celle passée en paramètre
   private getRandomShape(shape: any) {
-    var shapes = [
-      <SquareShape />,
-      <RectangleShape />,
-      <TriangleShape />,
-      <CircleShape />,
-      <DiamondShape />
+    const shapesWithoutShape = difference(shapes, [shape]);
+    return shapesWithoutShape[
+      Math.floor(Math.random() * shapesWithoutShape.length)
     ];
-    var rand = Math.floor(Math.random() * 5);
-    while (shapes[rand].type == shape.type) {
-      rand = Math.floor(Math.random() * 5);
-    }
-    return shapes[rand];
   }
 
   //Change le score en fonction de la paire sélectionnée
-  private setScore(diff: number, row: number) {
-    if (diff) {
-      this.setState({
-        score: this.state.score + 10
-      });
-    } else {
-      this.setState({
-        score: this.state.score - 10
-      });
-    }
-    window.console.log("before = " + this.state.isSelected)
-    this.state.isSelected[row] = true;
-    window.console.log("after = " + this.state.isSelected)  
-    }
+  private setScore(diff: number, id: number) {
+    let isLineSelected = this.state.isLineSelected;
+    isLineSelected[id] = true;
+    let score = this.state.score;
+    score = diff ? (score += 10) : (score -= 10);
+
+    window.console.log("before = " + this.state.isLineSelected);
+
+    this.setState({
+      isLineSelected: isLineSelected,
+      score
+    });
+
+    window.console.log("after = " + this.state.isLineSelected);
+  }
 }
 
 const styles = StyleSheet.create({
@@ -176,15 +150,18 @@ const styles = StyleSheet.create({
   correct: {
     flex: 1,
     flexDirection: "row",
+    backgroundColor: "lightgreen",
     borderStyle: "solid",
-    borderColor: "green"
+    borderColor: "green",
+    borderWidth: 1
   },
 
   wrong: {
     flex: 1,
     flexDirection: "row",
     borderStyle: "solid",
-    borderColor: "red"
+    borderColor: "red",
+    borderWidth: 1
   },
 
   default: {
